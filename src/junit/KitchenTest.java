f1110ae59c44e4d568e366b5ec14ff17b046b6f8
Wann-Ting Tu (s3846375) <S3846375@student.rmit.edu.au>
2024-05-08 15:56:44 +1000

    add junit testing

---
+++ src/junit/KitchenTest.java
@@ -0,0 +1,68 @@
+package junit;
+
+
+import partB.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KitchenTest {
+
+    Kitchen kitchen;
+    Order order;
+    Food burrito;
+    Food fries;
+    Food soda;
+
+    @Before
+    public void setUp() throws Exception {
+        kitchen = new Kitchen();
+        burrito = new Burrito("Burrito", 7);
+        fries = new Fries("Fries", 4);
+        soda = new Soda("Soda", 2.5);
+    }
+
+    @Test
+    public void prepareTimeWithNoUnsoldFries() {
+        // final order has 3 Burrito, 1 Fries
+        order = new Order();
+        order.addtoOrder(burrito, 3);
+        order.addtoOrder(fries, 1);
+        order.addtoOrder(soda, 1);
+
+        assertEquals(18, kitchen.getPrepareTime(order), 0);
+    }
+
+    @Test
+    public void prepareTimeWithUnsoldFries() {
+        // first order has 3 Burrito, 1 Fries, 1 soda
+        order = new Order();
+        order.addtoOrder(burrito, 3);
+        order.addtoOrder(fries, 1);
+        kitchen.getPrepareTime(order);
+
+        // second order has 2 Burrito, 12 Fries
+        order = new Order();
+        order.addtoOrder(burrito, 2);
+        order.addtoOrder(fries, 12);
+
+        assertEquals(16, kitchen.getPrepareTime(order), 0);
+    }
+
+    @Test
+    public void finalUnsoldFries() {
+        // first order has 3 Burrito, 1 Fries, 1 soda
+        order = new Order();
+        order.addtoOrder(burrito, 3);
+        order.addtoOrder(fries, 1);
+        kitchen.getPrepareTime(order);
+
+        // second order has 2 Burrito, 12 Fries
+        order = new Order();
+        order.addtoOrder(burrito, 2);
+        order.addtoOrder(fries, 12);
+        kitchen.getPrepareTime(order);
+
+        assertEquals(2, kitchen.getUnsoldFries(), 0);
+    }
+}
---
+++ src/junit/OrderTest.java
@@ -0,0 +1,55 @@
+package junit;
+
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+import partB.*;
+
+import static org.junit.Assert.*;
+
+public class OrderTest {
+    List<Food> foods;
+    Order order;
+
+    @Before
+    public void setUp() throws Exception {
+        foods = new ArrayList<>();
+        foods.add(new Burrito("Burrito", 7));
+        foods.add(new Fries("Fries", 4));
+        foods.add(new Soda("Soda", 2.5));
+    }
+
+    @Test
+    public void totalCostWithoutMeal() {
+        //order 1 Burrito, 2 Fries
+        order = new Order();
+        order.addtoOrder(foods.get(0), 1);
+        order.addtoOrder(foods.get(1), 2);
+        assertEquals(15, order.getTotalCost(), 0);
+    }
+
+    @Test
+    public void totalCostOrderSameFood() {
+        //order 1 Burrito, 1 Fries, 1 Soda, 2 Burrito
+        order = new Order();
+        order.addtoOrder(foods.get(0), 1);
+        order.addtoOrder(foods.get(1), 1);
+        order.addtoOrder(foods.get(2), 1);
+        order.addtoOrder(foods.get(0), 2);
+        assertEquals(27.5, order.getTotalCost(), 0);
+    }
+
+    @Test
+    public void totalCostWithMeal() {
+        //order 1 Burrito, 5 Fries, 2 Meal
+        order = new Order();
+        order.addtoOrder(foods.get(0), 1);
+        order.addtoOrder(foods.get(1), 5);
+        order.addMealCount(2);
+        for (Food f : foods) {
+            order.addtoOrder(f, 2);
+        }
+        assertEquals(48, order.getTotalCost(), 0);
+    }
+
+}
